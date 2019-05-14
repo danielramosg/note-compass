c64fa13d6f6b9878237ff1b377342013b6c341e7
Daniel Ramos <daniel.ramosg@gmail.com>
2019-05-14 11:30:44 +0200

    Text, scale-keyboard and piano updated

--- tonk.js
+++ tonk.js
@@ -1,21 +1,35 @@
 const mod = (x, n) => (x % n + n) % n
 
 TkData =[
-{indices: [0, 36, 72, 24, 60, 12, 48], notes: ['C', 'G', 'D', 'A', 'E', 'B', 'F#'], signature: 1, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']},  
-{indices: [49, 1, 37, 73, 25, 61, 13], notes: ['F', 'C', 'G', 'D', 'A', 'E', 'B'], signature: 0, all_note_names: ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']},  
-{indices: [14, 50, 2, 38, 74, 26, 62], notes: ['Bb', 'F', 'C', 'G', 'D', 'A', 'E'], signature: -1, all_note_names: ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']}, 
-{indices: [63, 15, 51, 3, 39, 75, 27], notes: ['Eb', 'Bb', 'F', 'C', 'G', 'D', 'A'], signature: -2, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']}, 
-{indices: [28, 64, 16, 52, 4, 40, 76], notes: ['Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D'], signature: -3, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']}, 
-{indices: [77, 29, 65, 17, 53, 5, 41], notes: ['Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G'], signature: -4, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'Cb']}, 
-{indices: [42, 78, 30, 66, 18, 54, 6], notes: ['Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C'], signature: -5, all_note_names: ['C', 'Db', 'D', 'Eb', 'Fb', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'Cb']}, 
-{indices: [7, 43, 79, 31, 67, 19, 55], notes: ['B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#'], signature: 6, all_note_names: ['B#', 'C#', 'Cx', 'D#', 'E', 'E#', 'F#', 'Fx', 'G#', 'A', 'A#', 'B']}, 
-{indices: [56, 8, 44, 80, 32, 68, 20], notes: ['E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#'], signature: 5, all_note_names: ['B#', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'Fx', 'G#', 'A', 'A#', 'B']}, 
-{indices: [21, 57, 9, 45, 81, 33, 69], notes: ['A', 'E', 'B', 'F#', 'C#', 'G#', 'D#'], signature: 4, all_note_names: ['B#', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'G', 'G#', 'A', 'A#', 'B']}, 
-{indices: [70, 22, 58, 10, 46, 82, 34], notes: ['D', 'A', 'E', 'B', 'F#', 'C#', 'G#'], signature: 3, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'G', 'G#', 'A', 'A#', 'B']}, 
-{indices: [35, 71, 23, 59, 11, 47, 83], notes: ['G', 'D', 'A', 'E', 'B', 'F#', 'C#'], signature: 2, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']}
+{indices: [0, 36, 72, 24, 60, 12, 48], notes_id: [0,7,2,9,4,11,6], notes: ['C', 'G', 'D', 'A', 'E', 'B', 'F#'], signature: 1, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']},  
+{indices: [49, 1, 37, 73, 25, 61, 13], notes_id: [5,0,7,2,9,4,11], notes: ['F', 'C', 'G', 'D', 'A', 'E', 'B'], signature: 0, all_note_names: ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']},  
+{indices: [14, 50, 2, 38, 74, 26, 62], notes_id: [10,5,0,7,2,9,4], notes: ['Bb', 'F', 'C', 'G', 'D', 'A', 'E'], signature: -1, all_note_names: ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']}, 
+{indices: [63, 15, 51, 3, 39, 75, 27], notes_id: [3,10,5,0,7,2,9], notes: ['Eb', 'Bb', 'F', 'C', 'G', 'D', 'A'], signature: -2, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']}, 
+{indices: [28, 64, 16, 52, 4, 40, 76], notes_id: [8,3,10,5,0,7,2], notes: ['Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D'], signature: -3, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']}, 
+{indices: [77, 29, 65, 17, 53, 5, 41], notes_id: [1,8,3,10,5,0,7], notes: ['Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G'], signature: -4, all_note_names: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'Cb']}, 
+{indices: [42, 78, 30, 66, 18, 54, 6], notes_id: [6,1,8,3,10,5,0], notes: ['Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C'], signature: -5, all_note_names: ['C', 'Db', 'D', 'Eb', 'Fb', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'Cb']}, 
+{indices: [7, 43, 79, 31, 67, 19, 55], notes_id: [11,6,1,8,3,10,5], notes: ['B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#'], signature: 6, all_note_names: ['B#', 'C#', 'Cx', 'D#', 'E', 'E#', 'F#', 'Fx', 'G#', 'A', 'A#', 'B']}, 
+{indices: [56, 8, 44, 80, 32, 68, 20], notes_id: [4,11,6,1,8,3,10], notes: ['E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#'], signature: 5, all_note_names: ['B#', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'Fx', 'G#', 'A', 'A#', 'B']}, 
+{indices: [21, 57, 9, 45, 81, 33, 69], notes_id: [9,4,11,6,1,8,3], notes: ['A', 'E', 'B', 'F#', 'C#', 'G#', 'D#'], signature: 4, all_note_names: ['B#', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'G', 'G#', 'A', 'A#', 'B']}, 
+{indices: [70, 22, 58, 10, 46, 82, 34], notes_id: [2,9,4,11,6,1,8], notes: ['D', 'A', 'E', 'B', 'F#', 'C#', 'G#'], signature: 3, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'E#', 'F#', 'G', 'G#', 'A', 'A#', 'B']}, 
+{indices: [35, 71, 23, 59, 11, 47, 83], notes_id: [7,2,9,4,11,6,1], notes: ['G', 'D', 'A', 'E', 'B', 'F#', 'C#'], signature: 2, all_note_names: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']}
 ];
 
 
+/*
+Each row has the following structure: 
+indices 			a list of those 7 indices (out of 0, …, 83) to which this row applies
+notes_id			a list of the 7 notes encoded as numbers: 
+notes				a chain of 7 note names from the line of fifths. The 7 names belong to the actual white notes of the chosen mode, 
+      				and each index in (indices) is positioned among the 7 indices in such a way that the note-name in (notes_id) and (notes) 
+      				marks the 1st degree of that mode. 
+signature			an integer between -5 and 6 denoting the key signature (see snapshot)
+all_note_names		a list of note names for the 12 pitches in chromatic order. 
+
+Thus, we need to permute cyclically the array notes_id so that the 1st note in the scale is the one in the position of the selected index.
+*/
+
+//Array.prototype.rotate = function(n) { return this.slice(n, this.length).concat(this.slice(0, n)); }
 
 mode = TkData[1]
 
@@ -23,18 +37,19 @@ mode = TkData[1]
 
 
 var pitchNotes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
-var solfaNotes = ["fa", "do", "so", "re", "la", "mi", "ti", "", "", "", "", "" ];
+var solfaNotes = ["fa", "do", "so", "re", "la", "mi", "ti", "fi \xa0 sa", "di \xa0 ra", "si \xa0 lo", "ri \xa0 ma", "li \xa0 ta" ];
 var solfaNotesFile = ["Fa", "Do", "So", "Re", "La", "Mi", "Ti", "", "", "", "", "" ]; //for mp3 filenames
 
 var modeNames = [
-{name:"lydian", color:"black", fullname:"Lydian"},
-{name:"ionian", color:"white", fullname:"Ionian"},
-{name:"mixolyd.", color:"black", fullname:"Mixolydian"},
-{name:"dorian", color:"black", fullname:"Dorian"},
-{name:"aeolian", color:"white", fullname:"Aeolian"},
-{name:"phrygian", color:"black", fullname:"Phrygian"},
-{name:"locrian", color:"grey", fullname:"Locrian"}
+{name:"lydian", color:"black", fullname:"Lydian", pattern:"2,2,2,1,2,2,1"},
+{name:"ionian", color:"white", fullname:"Ionian", pattern:"2,2,1,2,2,2,1"},
+{name:"mixolyd.", color:"black", fullname:"Mixolydian", pattern:"2,2,1,2,2,1,2"},
+{name:"dorian", color:"black", fullname:"Dorian", pattern:"2,1,2,2,2,1,2"},
+{name:"aeolian", color:"white", fullname:"Aeolian", pattern:"2,1,2,2,1,2,2"},
+{name:"phrygian", color:"black", fullname:"Phrygian", pattern:"1,2,2,2,1,2,2"},
+{name:"locrian", color:"grey", fullname:"Locrian", pattern:"1,2,2,1,2,2,2"}
 ];
+
 
 Scale = {
 
@@ -99,9 +114,15 @@ notes : [
 		],
 
 setValues : function(position){
+			//console.log("settedValues")
+			//mode = TkData.filter(function(m) {return m.indices.includes(mod(position,84)) } )[0]
 			var pitchNotes = mode.all_note_names;
 
-			var rootSector= 2*Math.floor(position/12.) + ( position%12 > 6 ? 1 : 0);
+			//var index_position = mode.indices.indexOf( mod(position,84) )
+			//var scale = mode.notes_id.rotate(index_position)
+			//console.log(scale)
+
+			//var rootSector= 2*Math.floor(position/12.) + ( position%12 > 6 ? 1 : 0);
 
 			this.notes.forEach(function(d,i){ 
 				d.id = i;
@@ -110,9 +131,15 @@ setValues : function(position){
 					d.nSolfa 	= mod(i+position,7)
 					d.Solfa 	= solfaNotesFile[d.nSolfa]
 					d.nLiteral 	= mod( i - Math.floor( (i+position)/7. ) , 12 )
+					//d.nLiteral = scale[i/2]
 					d.Literal 	= pitchNotes[d.nLiteral]
-					d.midiPitch = 60 + d.nLiteral
-					if (i<rootSector) d.midiPitch -=12;
+					d.midiPitch = 50 + d.nLiteral
+
+					if (i==0) basePitch = d.midiPitch
+					if (d.midiPitch < basePitch) {d.midiPitch+=12}
+					//if (i<rootSector) d.midiPitch -=12;
+
+					//console.log(d.nLiteral , d.midiPitch)
 		
 					var url = "./SolfedgeSamplesMp3/" + d.midiPitch + '_' + d.Solfa + ".mp3"
 					d.playerSolfa = new Tone.Player({
@@ -133,13 +160,16 @@ setValues : function(position){
 							d.active	= true
 							d.nLiteral 	= mod( i - Math.floor( (i+position)/7. ) , 12 )
 							d.Literal 	= pitchNotes[d.nLiteral]
-							d.midiPitch = 60 + d.nLiteral
-							if (i<rootSector) d.midiPitch -=12;
+							d.midiPitch = 50 + d.nLiteral
+							if (d.midiPitch < basePitch) {d.midiPitch+=12}
+							//if (i<rootSector) d.midiPitch -=12;
 					}
 					}	
 				})
 
 			}
+
+
 
 
 }
@@ -148,14 +178,23 @@ setValues : function(position){
 function updateModeLabel(){
 	var note = Scale.notes[0].Literal
 	var mode = modeNames[mod(position,7)].fullname
+	var pattern = modeNames[mod(position,7)].pattern
+	var solfa = Scale.notes[0].Solfa
+
+
 	d3.select("#modeLabel").text(note+ ' - ' +mode);
+
+	d3.select("#modeText").html(
+		"The scale starts in " + note + " and the intervals between consecutive notes are " + pattern + " semitones (" + mode + "). <br>" + 
+		"The note " + note + " plays the role of " + solfa + " in Solfedge."
+		)	
 }
 
 
 ////// Wheel 
 
-var height = 600;
-var width = 900;
+//var height = 600;
+//var width = 900;
 
 
 function sectorPath(theta, r, R){		//returns path for an arc of circle ring
@@ -214,7 +253,7 @@ sectorsMark.append("g") //Sectors
 		.on("mousedown", playStart)
 		.on("mouseup",playStop);
 
-sectorsMark.filter(function(d,i) {return i==0} )
+sectorsMark.filter(function(d,i) {return i==0} ) //Mode names
 	.append("g")		
 	.selectAll(".modeName")
 	.data(modeNames).enter()
@@ -225,10 +264,10 @@ sectorsMark.filter(function(d,i) {return i==0} )
 	.attr("transform",function(d,i){return "rotate(" + ((i+1)*stepAngle-1) +')' });
 
 
-sectorsMark.filter(function(d,i) {return i==0} )
+sectorsMark.filter(function(d,i) {return i==0} ) //Tick lines for Mode names
 	.selectAll(".solfaTick").data(d3.range(8)).enter()
 	.append("line")
-	.attr("x1",220).attr("y1",0)
+	.attr("x1",.68*Radius).attr("y1",0)
 	.attr("x2",Radius).attr("y2",0)
 	.attr("class","solfaTick")
 	.attr("transform",function(d,i){return "rotate(" + (i*stepAngle) +')' });
@@ -239,7 +278,7 @@ sectorsMark.filter(function(d,i) {return (i % 2 == 0 && i>0) } ) // scale number
 	.append("text").text(function(d,i){return (i+2) })
 	.attr("x",Radius*0.92)
 	.attr("text-anchor","end")
-	.attr("transform", "rotate(" + (3.5*stepAngle+2.5) +')' )
+	.attr("transform", "rotate(" + (3.5*stepAngle) +')' )
 	.attr("class","scaleNumbers")
 
 
@@ -249,18 +288,9 @@ sectorsMark.attr("transform",function(d,i){
 								"rotate(" + (-90+stepAngle* (12*((i-1)/2) +7)) +')'  }
 		)
 
-var synth = new Tone.Synth().toMaster();
 
 
 
-function playStart(d,i){	
-	d.playerSolfa.start();
-	//synth.triggerAttackRelease(d.Literal+"4", "8n");
-}
-
-function playStop(d){
-	d.playerSolfa.stop();
-}
 
 
 solfaMark = outerWheel.selectAll(".solfaMark")
@@ -268,14 +298,14 @@ solfaMark = outerWheel.selectAll(".solfaMark")
 	.append("g").attr("class","solfaMark")
 
 solfaMark.append("text")	
-	.attr("x",240)
+	.attr("x",function(d,i){var r = (d%12)/12 ; return (0.71 *r + 0.77 * (1-r) )*Radius })
 	.attr("y",0)
 	.text(function(d){return solfaNotes[d%12]})
 	.attr("transform",function(d,i){return "rotate(-1)" } );
 
 solfaMark.append("line")
-	.attr("x1",220).attr("y1",0)
-	.attr("x2",270).attr("y2",0)
+	.attr("x1",.68*Radius).attr("y1",0)
+	.attr("x2",.86*Radius).attr("y2",0)
 	.attr("class","wheel");	
 
 solfaMark.attr("transform",function(d,i){return "rotate(" + (-90+stepAngle*(i+1)) +')' } );
@@ -295,7 +325,7 @@ Star = scaleWheel.append("g")
 
 
 function updateStar(){
-	var radius = 225;
+	var radius = 0.7*Radius;
 	var theta = 360/12 * 7;
 
 		var points=[];
@@ -322,8 +352,20 @@ function updateStar(){
 
 }
 
-
 updateStar()
+
+
+// var scores= Star.append("g")
+// 				.attr("class","score")
+
+
+// score_heights=[0,1,2,3,4,7,8,9,10,11] 
+// score_heights.forEach(function(i){
+// 			scores.append("circle")
+// 			.attr("r",130+4*i)
+// 			})
+
+	
 
 
 
@@ -342,16 +384,38 @@ function updateStarText(data){
 	//console.log(noteMark)
 
 	noteMark.select("text").text(function(d){return d})
-		.attr("x",140)
+		.attr("x",.45*Radius)
 		.attr("y",0)
 		.attr("transform",function(d,i){return "rotate(2)" } );
 
 	noteMark.attr("transform",function(d,i){return "rotate(" + (-90+stepAngle*(7*i)) +')' } );
 
+
+	// scores.selectAll("text").data(data)
+	// 		.enter().append("text")
+	// 			.attr("class","scoreText")
+	// 			.text(function(d,i){return "&"})
+	// 			.attr("transform",function(d,i)
+	// 				{return "rotate(" + (-6-90+stepAngle*(7*i)) +') ' +
+	// 				"translate(162 0) " + 
+	// 				"rotate(90) "} );
+
+
+
+
 }
 			
 
 
+
+function playStart(d,i){	
+	d.playerSolfa.start();
+	//synth.triggerAttackRelease(d.Literal+"4", "8n");
+}
+
+function playStop(d){
+	d.playerSolfa.stop();
+}
 
 
 
@@ -404,6 +468,7 @@ function updateAll(){
 	updateKeyboard(WhiteScale)
 	updateStaff()
 	updateModeLabel()
+	updatePiano()
 }
 
 
@@ -459,8 +524,11 @@ d3.select("#playIcon").on("click", playScale );
 
 
 svg2 = d3.select("#keyboard").append("svg")
-		.attr("width",400)
-		.attr("height",300);
+		.attr("width",631)
+		.attr("height",150);
+
+
+var synth = new Tone.Synth().toMaster();
 
 
 function updateKeyboard(data){
@@ -474,46 +542,138 @@ var keys = svg2.selectAll("g").data(data);
 	//Enter selection
 	var newKey = keys.enter().append("g");
 
-		
-	newKey.append("rect")
-				.style("stroke-width",2)
-				.style("stroke","black")
-				.on("click",function (d) {synth.triggerAttackRelease(Tone.Frequency(d.midiPitch,"midi"),"8n") } )
-				.on("touchstart",function (d) {synth.triggerAttackRelease(Tone.Frequency(d.midiPitch,"midi"),"8n") } )
 	
-	newKey.append("text")
-				.text(function (d) {return d.Literal})
-				.attr("font-family", "sans-serif")
-	            .attr("font-size", "20px")		
-			
-	newKey.append("path")
-				.style("stroke-width",2)
-				.style("stroke","black")
-				.style("fill","none");
-			
-	// newKey.append("text");	
-								
 		
-	//Update selection			
-	keys = keys.merge(newKey);
+		
+		newKey.append("rect") 
+					.attr("class","oct1" )
+					.style("stroke-width",2)
+					.style("stroke","black")
+					.on("click",function (d) {synth.triggerAttackRelease(Tone.Frequency( (d.midiPitch ) ,"midi"),"8n") } )
+					.on("touchstart",function (d) {synth.triggerAttackRelease(Tone.Frequency( (d.midiPitch ),"midi"),"8n") } )
+					.attr("width",function (d) {return d.type=="white" ? 46 : 50 })
+					.attr("height",function (d) {return d.type=="white" ? 150 : 50 })
+					.style("fill",function (d) {return d.sectorColor})
+					.attr("x", function(d,i) {return 45* (i ) })
+					.attr("y", 0);
+		
+		newKey.append("rect") 
+					.attr("class","oct2")
+					.style("stroke-width",2)
+					.style("stroke","black")
+					.on("click",function (d) {synth.triggerAttackRelease(Tone.Frequency(d.midiPitch + 12,"midi"),"8n") } )
+					.on("touchstart",function (d) {synth.triggerAttackRelease(Tone.Frequency(d.midiPitch + 12,"midi"),"8n") } )
+					.attr("width",function (d) {return d.type=="white" ? 46 : 50 })
+					.attr("height",function (d) {return d.type=="white" ? 150 : 50 })
+					.style("fill",function (d) {return d.sectorColor})
+					.attr("x", function(d,i) {return 45* (i + 7) })
+					.attr("y", 0);			
+		
+		newKey.append("text")
+					.attr("class","key-Ordinal")
+					.attr("text-anchor","middle")
+					.text(function (d) {return (d.id/2 + 1) })
+					.attr("x", function(d,i) {return 22+ 45* (i) })
+					.attr("y", 20)	
 
+		newKey.append("text")
+					.attr("class","key-Ordinal")
+					.attr("text-anchor","middle")
+					.text(function (d) {return (d.id/2 + 1) })
+					.attr("x", function(d,i) {return 22+ 45* (i + 7) })
+					.attr("y", 20)	
+
+		newKey.append("text")
+					.attr("class","key-Literal")
+					.attr("text-anchor","middle")
+					.text(function (d) {return d.Literal})
+					.attr("x", function(d,i) {return 22+ 45* (i) })
+					.attr("y", 55)	
+
+		newKey.append("text")
+					.attr("class","key-Literal")
+					.attr("text-anchor","middle")
+					.text(function (d) {return d.Literal})
+					.attr("x", function(d,i) {return 22+ 45* (i + 7) })
+					.attr("y", 55)					
 		
-	keys.select("rect")
-				.attr("width",function (d) {return d.type=="white" ? 50 : 50 })
-				.attr("height",function (d) {return d.type=="white" ? 150 : 50 })
-				.style("fill",function (d) {return d.sectorColor})
-				.attr("x", function(d,i) {return 55* i })
-				.attr("y", 25);
+		newKey.append("text")
+					.attr("class","key-Solfa")
+					.attr("text-anchor","middle")
+					.text(function (d) {return d.Solfa})
+					.attr("x", function(d,i) {return 22+ 45* (i) })
+					.attr("y", 80)	
+
+		newKey.append("text")
+					.attr("class","key-Solfa")
+					.attr("text-anchor","middle")
+					.text(function (d) {return d.Solfa})
+					.attr("x", function(d,i) {return 22+ 45* (i + 7) })
+					.attr("y", 80)	
+
+
 	
-	keys.select("text")
-				.text(function (d) {return d.Literal})
-				.attr("x", function(d,i) {return 15+ 55* i })
-				.attr("y", 20)	
+				
+		// newKey.append("text");	
+									
+			
+		//Update selection			
+		keys = keys.merge(newKey);
 
+			
+		// keys.select("rect")
+		// 			.attr("width",function (d) {return d.type=="white" ? 50 : 50 })
+		// 			.attr("height",function (d) {return d.type=="white" ? 150 : 50 })
+		// 			.style("fill",function (d) {return d.sectorColor})
+		// 			.attr("x", function(d,i) {return 55* (i + 7*oct) })
+		// 			.attr("y", 25);
+		
+		keys.selectAll("text.key-Ordinal")
+					.text(function (d) {return (d.id/2 + 1) })
+
+		keys.selectAll("text.key-Literal")
+					.text(function (d) {return d.Literal})
+
+		keys.selectAll("text.key-Solfa")
+					.text(function (d) {return d.Solfa})
+	
 	
 }
 
 
+
+
+///// Piano
+
+
+
+
+JZZ.synth.Tiny.register();
+
+piano = JZZ.input.Kbd(
+		{at:'piano',
+		from: 'C4',
+		to: 'B5',
+		ww: 45,
+		wl: 150,
+		}
+	)	
+.connect(JZZ().openMidiOut());
+
+function updatePiano(){	
+	piano.getWhiteKeys().setStyle({ backgroundColor: 'white' }, { });
+	piano.getBlackKeys().setStyle({ backgroundColor: 'black' }, { });
+
+
+	Scale.notes.forEach(function(d,i){
+		if (d.type == "white") {	
+			piano.getKey(d.Literal+'4').setStyle({ backgroundColor: d.sectorColor}, {});
+			piano.getKey(d.Literal+'5').setStyle({ backgroundColor: d.sectorColor }, { });
+		}
+	})
+}
+
+updatePiano();
 
 
 ///// staff
@@ -569,4 +729,9 @@ function updateStaff () {
 }
 
 updateAll()
+
+
+
+
+
 
