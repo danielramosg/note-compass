7898621e01e15ab0e0f7872d3108363ec00872c8
Daniel Ramos <daniel.ramosg@gmail.com>
2024-05-28 11:51:42 +0200

    Add loading banner

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const mod = (x, n) => (x % n + n) % n;
+const mod = (x, n) => ((x % n) + n) % n;
 
 // Global variables and setup
-position=1;
+position = 1;
 mode = {};
 getMode();
 
@@ -9,10 +9,10 @@ Scale.setValues(position);
 
 start();
 
-
-
-async function start(){
+async function start() {
+  loadingBanner(true);
   await loadAudio();
+  loadingBanner(false);
   drawWheel();
   loadSVGstaff();
   makeWhiteScaleLine(15);
@@ -21,57 +21,61 @@ async function start(){
   updateAll();
 }
 
+function updateAll() {
+  getMode();
 
-function updateAll(){
-	getMode();
-
-	rotateScaleWheel()
-	FullScale = Scale.notes.filter(function(d){return d.active})
-  WhiteScaleLine =[]
+  rotateScaleWheel();
+  FullScale = Scale.notes.filter(function (d) {
+    return d.active;
+  });
+  WhiteScaleLine = [];
   makeWhiteScaleLine(15);
 
-	updateStarText(mode.all_note_names)
-	updateKeyboard(WhiteScaleLine)
-	updateStaff()
-	updateModeLabel()
-	updatePiano()
+  updateStarText(mode.all_note_names);
+  updateKeyboard(WhiteScaleLine);
+  updateStaff();
+  updateModeLabel();
+  updatePiano();
 }
 
-
-
-
-
-
+function loadingBanner(active) {
+  if (active) {
+    var bg = d3
+      .select("body")
+      .append("div")
+      .attr("id", "loadingBanner")
+      .html(`Loading... this may take a few seconds.`);
+  } else {
+    d3.select("#loadingBanner").remove();
+  }
+}
 
 ///// Piano
 
 JZZ.synth.Tiny.register();
 var piano;
 
-function drawPiano(){
-  piano = JZZ.input.Kbd(
-  		{at:'piano',
-  		from: 'C3',
-  		to: 'C5',
-  		ww: 45,
-  		wl: 150,
-  		}
-  	)
-  .connect(JZZ().openMidiOut());
+function drawPiano() {
+  piano = JZZ.input
+    .Kbd({ at: "piano", from: "C3", to: "C5", ww: 45, wl: 150 })
+    .connect(JZZ().openMidiOut());
 }
 
+function updatePiano() {
+  piano.getWhiteKeys().setStyle({ backgroundColor: "white" }, {});
+  piano.getBlackKeys().setStyle({ backgroundColor: "black" }, {});
 
-function updatePiano(){
-	piano.getWhiteKeys().setStyle({ backgroundColor: 'white' }, { });
-	piano.getBlackKeys().setStyle({ backgroundColor: 'black' }, { });
-
-
-	Scale.notes.forEach(function(d,i){
-		if (d.type == "white") {
-			piano.getKey(d.Literal+'3').setStyle({ backgroundColor: d.sectorColor}, {});
-			piano.getKey(d.Literal+'4').setStyle({ backgroundColor: d.sectorColor }, { });
-			piano.getKey(d.Literal+'5').setStyle({ backgroundColor: d.sectorColor }, { });
-
-		}
-	})
+  Scale.notes.forEach(function (d, i) {
+    if (d.type == "white") {
+      piano
+        .getKey(d.Literal + "3")
+        .setStyle({ backgroundColor: d.sectorColor }, {});
+      piano
+        .getKey(d.Literal + "4")
+        .setStyle({ backgroundColor: d.sectorColor }, {});
+      piano
+        .getKey(d.Literal + "5")
+        .setStyle({ backgroundColor: d.sectorColor }, {});
+    }
+  });
 }
